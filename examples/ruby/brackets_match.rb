59b9b5f9a6e16576817dc62fd9e3dba093bfed0e
Suyog Nerkar MC <suyogrnerkar@gmail.com>
2018-06-15 20:00:58 -0400

    Javascript : Brackets match added.

--- examples/ruby/brackets_match.rb
+++ examples/ruby/brackets_match.rb
@@ -1,3 +1,7 @@
+--------------------------------------------------------------------------------
+  # Brackets Match : Ruby 
+--------------------------------------------------------------------------------
+
 # Given a string with any combination of the following brackets: "(){}[]".
 #
 # The output should determine true/false depening on
@@ -75,3 +79,54 @@ puts brackets_check? '{}['
 puts brackets_check? '{[}]'
 puts brackets_check? '}[{]'
 puts brackets_check? '({}})'
+
+
+--------------------------------------------------------------------------------
+  # Brackets Match : Javascript 
+--------------------------------------------------------------------------------
+
+function brackets_match(values) {
+    if(isOdd(values.length)){
+        return false;
+    }
+    
+    /*  
+      If we match any other character than the allowed ones,
+      i.e. we have some garbage that should not be pushed to 
+      stack. So we just terminate for simplicity purpose.
+        
+      [^\[\]\(\)\{\}] : ^ in regex matches any character other  
+                        than one of {, }, [, ], (, ) 
+    */
+  
+    if(values.match(/[^\[\]\(\)\{\}]/)) {
+      return false;
+    }
+    
+    let brackets = {'{' : '}', '(' : ')', '[' : ']'};
+    let stack = [], expected;
+  
+    for (let i = 0; i < values.length; i++) {
+      
+      if (values[i] in brackets) {
+          stack.push(brackets[values[i]]);
+      }
+      else {
+        expected = stack.pop();
+        if(values[i] !== expected) {
+          return false;
+        }
+        
+      }
+    }
+  
+    return (stack.length == 0) ? true : false;
+  
+}
+
+function isOdd(num) {
+    return num%2 === 1;
+}
+
+console.log(brackets_match("{[](}"));
+console.log(brackets_match("{[]()}"));
